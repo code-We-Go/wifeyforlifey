2a26ae0b50e583663c0a477fb2c64f262d566a9f
wiigwiig10 <wiigwiig10@gmail.com>
2025-08-11 13:21:40 +0300

    annoncment

---
+++ components/AnnouncmentBar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AnnouncmentBar = () => {
+  return (
+    <div className=" bg-gray-creamey h-10 px-4 py-2 text-lovely">
+      <p className="text-center font-medium">
+        Lorem, ipsum dolor
+        {/* <a href="#" className="inline-block underline"> sit amet consectetur </a> */}
+      </p>
+    </div>
+  );
+};
+
+export default AnnouncmentBar;
