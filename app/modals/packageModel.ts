0317e4bbc8d4b06fc0c4503f0c00665da11d1bbe
wiigwiig10 <wiigwiig10@gmail.com>
2026-01-11 08:16:42 +0200

    Model

--- app/modals/packageModel.ts
+++ app/modals/packageModel.ts
@@ -20,7 +20,7 @@ const PackageSchema = new Schema<Ipackage>(
       required: true,
       default: [],
     },
-    active: { type: Boolean, required: true, default: false },
+    active: { type: Boolean, default: false },
     notes: { type: [String], required: true, default: [] },
     cards: { type: [PackageCardSchema], required: false, default: [] },
   },
