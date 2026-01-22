c0c459370dde7f98cd6ddf0306f5669830df2ef8
wiigwiig10 <wiigwiig10@gmail.com>
2026-01-22 13:33:33 +0200

    url

---
+++ app/miniSubscriptionVerification/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MiniSubscriptionVerification = () => {
+  return (
+    <div className='min-h-screen w-full bg-creamey'></div>
+  )
+}
+
+export default MiniSubscriptionVerification
