d453f6b02208fac97a853e194d010dbf54b72fc4
wiigwiig10 <wiigwiig10@gmail.com>
2026-01-24 18:41:31 +0200

    menu

--- app/miniSubscriptionVerification/page.tsx
+++ app/miniSubscriptionVerification/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MiniSubscriptionVerification = () => {
   return (
-    <div className='min-h-screen w-full bg-creamey'></div>
+    <div className='min-h-screen w-full bg-creamey flex justify-center items-center text-lovely font-bold text-2xl'><h2>working on it</h2></div>
   )
 }
 
